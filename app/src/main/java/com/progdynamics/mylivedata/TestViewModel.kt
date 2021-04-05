b8d5d0ca096dcdf31ac8a45f342b73e5a1bd0b9b
Eduardo2020Br <edmanzini44@gmail.com>
2021-04-04 22:12:46 -0300

    Testing and Learning ViewModel and Live Data - Kotlin

---
+++ app/src/main/java/com/progdynamics/mylivedata/TestViewModel.kt
@@ -0,0 +1,14 @@
+package com.progdynamics.mylivedata
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TestViewModel : ViewModel() {
+    var number = 0
+    val currentNumber: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+    val currentBoolean: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+}
